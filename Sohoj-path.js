21a10a56243a28df7c74174716f685e386399442
Mahfazzalin Shawon Reza <74193300+Mahfazzalin@users.noreply.github.com>
2021-11-24 23:38:05 +0600

    Sohoj-path
    Sohoj-path

---
+++ Sohoj-path.js
@@ -0,0 +1,9 @@
+function openSideMenu(){
+            document.getElementById('side-menu').style.width = '250px';
+            document.getElementById('main').style.marginLeft = '250px';
+        }
+
+        function closeSideMenu(){
+            document.getElementById('side-menu').style.width = '1px';
+            document.getElementById('main').style.marginLeft = '1px';
+        }
